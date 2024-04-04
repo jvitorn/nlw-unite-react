72b7258ea8bf787b0153df7c7e7e72be9c19cf5a
jvitorn <joaoeuno2011@gmail.com>
2024-04-03 21:07:36 -0300

    aula02: estruturando componentes usando faker para consumir dados ficticios da aplicação e logica para paginação

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,22 @@
 import nwlIcon from "../assets/nlw-united-icon.svg"
 
+import { NavLink } from "./nav-link"
+
 export function Header() {
     return (
         <div className="flex items-center gap-5 py-2">
             <img src={nwlIcon} />
 
             <nav className="flex items-center gap-5">
-                <a href="" className="font-medium text-sm text-zinc-400">Eventos</a>
-                <a href="" className="font-medium text-sm">Participantes</a>
+                <NavLink href="/eventos"> 
+                    Eventos
+                </NavLink>
+                <NavLink href="/eventos"> 
+                    Participantes
+                </NavLink>
+                <NavLink href="/eventos"> 
+                    Link 3
+                </NavLink>
             </nav>
         </div>
     )
